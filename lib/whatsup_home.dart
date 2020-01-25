a432586615ff4738fed9893c45355e4044a3746c
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-24 19:10:19 -0500

    Create whatsup_home.dart

---
+++ lib/whatsup_home.dart
@@ -0,0 +1,85 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsup/pages/call_screen.dart';
+import 'package:whatsup/pages/camera_screen.dart';
+import 'package:whatsup/pages/chat_screen.dart';
+import 'package:whatsup/pages/status_screen.dart';
+
+class WhatsUpHome extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  WhatsUppHome({this.cameras});
+
+  @override
+  _WhatsUpHomeState createState() => _WhatsUpHomeState();
+}
+
+class _WhatsUpHomeState extends State<WhatsUpHome>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  bool showFab = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(vsync: this, initialIndex: 1, length: 4);
+    _tabController.addListener(() {
+      if (_tabController.index == 1) {
+        showFab = true;
+      } else {
+        showFab = false;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WhatsUp"),
+        elevation: 0.7,
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.camera_alt)),
+            Tab(text: "CHATS"),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(
+              text: "CALLS",
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          Icon(Icons.search),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0),
+          ),
+          Icon(Icons.more_vert)
+        ],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          CameraScreen(widget.cameras),
+          ChatScreen(),
+          StatusScreen(),
+          CallsScreen(),
+        ],
+      ),
+      floatingActionButton: showFab
+          ? FloatingActionButton(
+              backgroundColor: Theme.of(context).accentColor,
+              child: Icon(
+                Icons.message,
+                color: Colors.white,
+              ),
+              onPressed: () => print("open chats"),
+            )
+          : null,
+    );
+  }
+}
