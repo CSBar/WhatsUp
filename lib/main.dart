c88d496fea4c669de3af1717fe34622904fd2e77
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-24 20:22:51 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,29 @@ List<CameraDescription> cameras;
 Future<Null> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
+    return MaterialApp(
       title: "WhatsUp",
-      theme: new ThemeData(
-        primaryColor: new Color(0xff075E54),
-        accentColor: new Color(0xff25D366),
-        darkTheme: ThemeData.dark(),
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Color(0xff075E54),
+        accentColor: Color(0xff25D366),
+        backgroundColor: Colors.black54,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColorDark: Colors.black,
+        primaryColor: Color(0xff004242),
+        accentColor: Color(0xff25D366),
       ),
       debugShowCheckedModeBanner: false,
-      home: new WhatsUpHome(cameras:cameras),
+      home: WhatsUpHome(cameras:cameras),
     );
   }
 }
